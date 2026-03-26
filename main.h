a239b999cacd398074167cf3e2ebb58341481138
Pierre1200 <rouvellat.p@icloud.com>
2026-03-26 18:26:39 +0100

    Update function declarations and improve print_percent function for clarity

--- main.h
+++ main.h
@@ -22,5 +22,6 @@ int _printf(const char *format, ...);
 int print_char(va_list args);
 int print_string(va_list args);
 int print_percent(va_list args);
+int (*get_print_func(char s))(va_list);
 
 #endif
